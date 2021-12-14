c1f3ea6ade03af6af7abc69e755862e9f7ce50ed
sripad <sripad2708@gmail.com>
2021-12-14 02:51:25 -0500

    Trainer Class, lr scheduler, Dice sorted

--- utils.py
+++ utils.py
@@ -2,50 +2,149 @@ from torch.nn.modules.loss import _Loss
 import segmentation_models_pytorch as sm
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 
 
-class DiceLoss(_Loss):
-    def __init__(self,eps=1e-7):
-        super().__init__()
-        self.eps = eps
+class DiceScore(nn.Module):
+    r"""Criterion that computes Sørensen-Dice Coefficient loss.
+
+    According to [1], we compute the Sørensen-Dice Coefficient as follows:
+
+    .. math::
+
+        \text{Dice}(x, class) = \frac{2 |X| \cap |Y|}{|X| + |Y|}
+
+    where:
+       - :math:`X` expects to be the scores of each class.
+       - :math:`Y` expects to be the one-hot tensor with the class labels.
+
+    the loss, is finally computed as:
+
+    .. math::
+
+        \text{loss}(x, class) = 1 - \text{Dice}(x, class)
+
+    [1] https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+
+    Shape:
+        - Input: :math:`(N, C, H, W)` where C = number of classes.
+        - Target: :math:`(N, H, W)` where each value is
+          :math:`0 ≤ targets[i] ≤ C−1`.
+
+    Examples:
+        >>> N = 5  # num_classes
+        >>> loss = tgm.losses.DiceLoss()
+        >>> input = torch.randn(1, N, 3, 5, requires_grad=True)
+        >>> target = torch.empty(1, 3, 5, dtype=torch.long).random_(N)
+        >>> output = loss(input, target)
+        >>> output.backward()
+    """
+
+    def __init__(self) -> None:
+        super(DiceScore, self).__init__()
+        self.eps: float = 1e-6
+
+    def forward(
+            self,
+            input: torch.Tensor,
+            target: torch.Tensor) -> torch.Tensor:
+        if not torch.is_tensor(input):
+            raise TypeError("Input type is not a torch.Tensor. Got {}"
+                            .format(type(input)))
+        if not len(input.shape) == 4:
+            raise ValueError("Invalid input shape, we expect BxNxHxW. Got: {}"
+                             .format(input.shape))
+        if not input.shape[-2:] == target.shape[-2:]:
+            raise ValueError("input and target shapes must be the same. Got: {}"
+                             .format(input.shape, input.shape))
+        if not input.device == target.device:
+            raise ValueError(
+                "input and target must be in the same device. Got: {}" .format(
+                    input.device, target.device))
+        # compute softmax over the classes axis
+        input_soft = input#F.softmax(input, dim=1)
+
+        # create the labels one hot tensor
+        target_one_hot = torch.eye(2)[target.squeeze(1)]
+        target_one_hot = target_one_hot.permute(0, 3, 1, 2).float()
+
+        # compute the actual dice score
+        dims = (1, 2, 3)
+        intersection = torch.sum(input_soft * target_one_hot, dims)
+        cardinality = torch.sum(input_soft + target_one_hot, dims)
+
+        dice_score = 2. * intersection / (cardinality + self.eps)
+        return dice_score#torch.mean(1. - dice_score)
+
     
-    def forward(self,input:torch.Tensor,target:torch.Tensor)->torch.Tensor:
-        #cite https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
-        """Computes the Sørensen–Dice loss.
-        Note that PyTorch optimizers minimize a loss. In this
-        case, we would like to maximize the dice loss so we
-        return the negated dice loss.
-        Args:
-            true: a tensor of shape [B, 1, H, W].
-            logits: a tensor of shape [B, C, H, W]. Corresponds to
-                the raw output or logits of the model.
-            eps: added to the denominator for numerical stability.
-        Returns:
-            dice_loss: the Sørensen–Dice loss.
-        """
-        num_classes = input.shape[1]
-        if num_classes == 1:
-            true_1_hot = torch.eye(num_classes + 1)[target.squeeze(1)]
-            true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-            true_1_hot_f = true_1_hot[:, 0:1, :, :]
-            true_1_hot_s = true_1_hot[:, 1:2, :, :]
-            true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
-            pos_prob = torch.sigmoid(input)
-            neg_prob = 1 - pos_prob
-            probas = torch.cat([pos_prob, neg_prob], dim=1)
-        else:
-            true_1_hot = torch.eye(num_classes)[target.squeeze(1)]
-            true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-            probas = F.softmax(input, dim=1)
-        true_1_hot = true_1_hot.type(input.type())
-        dims = (0,) + tuple(range(2, target.ndimension()))
-        intersection = torch.sum(probas * true_1_hot, dims)
-        cardinality = torch.sum(probas + true_1_hot, dims)
-        dice_loss = (2. * intersection / (cardinality + self.eps)).mean()
-        return (1 - dice_loss)
-        
-        
-
     
+class DiceLoss(nn.Module):
+    r"""Criterion that computes Sørensen-Dice Coefficient loss.
 
-        
+    According to [1], we compute the Sørensen-Dice Coefficient as follows:
+
+    .. math::
+
+        \text{Dice}(x, class) = \frac{2 |X| \cap |Y|}{|X| + |Y|}
+
+    where:
+       - :math:`X` expects to be the scores of each class.
+       - :math:`Y` expects to be the one-hot tensor with the class labels.
+
+    the loss, is finally computed as:
+
+    .. math::
+
+        \text{loss}(x, class) = 1 - \text{Dice}(x, class)
+
+    [1] https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+
+    Shape:
+        - Input: :math:`(N, C, H, W)` where C = number of classes.
+        - Target: :math:`(N, H, W)` where each value is
+          :math:`0 ≤ targets[i] ≤ C−1`.
+
+    Examples:
+        >>> N = 5  # num_classes
+        >>> loss = tgm.losses.DiceLoss()
+        >>> input = torch.randn(1, N, 3, 5, requires_grad=True)
+        >>> target = torch.empty(1, 3, 5, dtype=torch.long).random_(N)
+        >>> output = loss(input, target)
+        >>> output.backward()
+    """
+
+    def __init__(self) -> None:
+        super(DiceLoss, self).__init__()
+        self.eps: float = 1e-6
+
+    def forward(
+            self,
+            input: torch.Tensor,
+            target: torch.Tensor) -> torch.Tensor:
+        if not torch.is_tensor(input):
+            raise TypeError("Input type is not a torch.Tensor. Got {}"
+                            .format(type(input)))
+        if not len(input.shape) == 4:
+            raise ValueError("Invalid input shape, we expect BxNxHxW. Got: {}"
+                             .format(input.shape))
+        if not input.shape[-2:] == target.shape[-2:]:
+            raise ValueError("input and target shapes must be the same. Got: {}"
+                             .format(input.shape, input.shape))
+        if not input.device == target.device:
+            raise ValueError(
+                "input and target must be in the same device. Got: {}" .format(
+                    input.device, target.device))
+        # compute softmax over the classes axis
+        input_soft = input#F.softmax(input, dim=1)
+
+        # create the labels one hot tensor
+        target_one_hot = torch.eye(2)[target.squeeze(1)]
+        target_one_hot = target_one_hot.permute(0, 3, 1, 2).float()
+
+        # compute the actual dice score
+        dims = (1, 2, 3)
+        intersection = torch.sum(input_soft * target_one_hot, dims)
+        cardinality = torch.sum(input_soft + target_one_hot, dims)
+
+        dice_score = 2. * intersection / (cardinality + self.eps)
+        return dice_score#torch.mean(1. - dice_score)
